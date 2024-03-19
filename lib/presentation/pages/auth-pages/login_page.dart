fdb24447214aa157f141d25d7f2d693f141f24f8
hoeskiee <hoeskiee@gmail.com>
2024-03-19 20:52:45 +0700

    Remove unused import

--- lib/presentation/pages/auth-pages/login_page.dart
+++ lib/presentation/pages/auth-pages/login_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
 import 'package:mitra_surya_jaya/presentation/misc/colors.dart';
