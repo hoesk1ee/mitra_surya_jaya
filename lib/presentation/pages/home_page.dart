59ff625d48b314884eb60671e44cb5aa075c4d90
hoeskiee <hoeskiee@gmail.com>
2024-03-25 11:07:13 +0700

    improve home UI

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:mitra_surya_jaya/presentation/misc/colors.dart';
 import 'package:mitra_surya_jaya/presentation/misc/text_styles.dart';
 
@@ -10,124 +9,223 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.fromLTRB(18, 24, 18, 0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  children: [
-                    const CircleAvatar(
-                      radius: 28,
-                    ),
-                    const Gap(12),
-                    RichText(
-                      text: TextSpan(
-                        text: "Selamat Pagi,",
-                        style: DefaultTextStyle.of(context).style.copyWith(
-                              height: 1.2,
-                              fontSize: 14,
-                            ),
-                        children: [
-                          TextSpan(
-                            text: "\nJohn Doe",
-                            style: AppTextStyle.titleTextStyle.copyWith(
-                              fontSize: 17,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                const Gap(22),
-                Container(
-                  width: MediaQuery.sizeOf(context).width,
-                  decoration: BoxDecoration(
-                    color: AppColor.mainOrangeColor,
-                    borderRadius: BorderRadius.circular(18),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(18, 24, 18, 18),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ///* User Profile Widget
+              Row(
+                children: [
+                  const CircleAvatar(
+                    radius: 28,
                   ),
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(22, 18, 22, 18),
-                    child: Row(
+                  const Gap(12),
+                  RichText(
+                    text: TextSpan(
+                      text: "Selamat Pagi,",
+                      style: DefaultTextStyle.of(context).style.copyWith(
+                            height: 1.2,
+                            fontSize: 14,
+                          ),
                       children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              "Total Omset",
-                              style: AppTextStyle.titleTextStyle.copyWith(
-                                color: AppColor.mainWhiteColor,
-                                fontSize: 21,
-                              ),
-                            ),
-                            RichText(
-                              text: TextSpan(
-                                text: "Rp ",
-                                style: AppTextStyle.titleTextStyle.copyWith(
-                                    fontFamily: "Poppins", fontSize: 14),
-                                children: [
-                                  TextSpan(
-                                    text: "20.000.000",
-                                    style: AppTextStyle.titleTextStyle.copyWith(
-                                      fontSize: 32,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
-                        const Gap(22),
-                        Expanded(
-                          child: Column(
-                            children: [
-                              Row(
-                                children: [
-                                  Container(
-                                    decoration: BoxDecoration(
-                                      shape: BoxShape.circle,
-                                      border: Border.all(
-                                        color: AppColor.profitColor,
-                                        width: 2,
-                                      ),
-                                    ),
-                                    child: const Icon(
-                                      Icons.expand_less,
-                                      color: AppColor.profitColor,
-                                    ),
-                                  ),
-                                  const Gap(8),
-                                  Text(
-                                    "20%",
-                                    style: AppTextStyle.titleTextStyle.copyWith(
-                                      color: AppColor.mainWhiteColor,
-                                      fontSize: 17,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              const Gap(4),
-                              Text(
-                                "Dibanding Kemarin",
-                                style: AppTextStyle.titleTextStyle.copyWith(
-                                  color: AppColor.mainWhiteColor,
-                                  fontSize: 12,
-                                ),
-                              )
-                            ],
+                        TextSpan(
+                          text: "\nJohn Doe",
+                          style: AppTextStyle.titleTextStyle.copyWith(
+                            fontSize: 17,
                           ),
                         ),
                       ],
                     ),
                   ),
-                )
-              ],
-            ),
+                ],
+              ),
+
+              ///* Turnover Card Widget
+              const Gap(22),
+              _buildCard(
+                context,
+                Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Omset Hari Ini",
+                          style: AppTextStyle.titleTextStyle.copyWith(
+                            color: AppColor.mainWhiteColor,
+                            fontSize: 17,
+                          ),
+                        ),
+                        RichText(
+                          text: TextSpan(
+                            text: "Rp",
+                            style: AppTextStyle.titleTextStyle
+                                .copyWith(fontFamily: "Poppins", fontSize: 14),
+                            children: [
+                              TextSpan(
+                                text: "20.000.000",
+                                style: AppTextStyle.titleTextStyle.copyWith(
+                                  fontSize: 28,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                    const Gap(22),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  border: Border.all(
+                                    color: AppColor.profitColor,
+                                    width: 1,
+                                  ),
+                                ),
+                                child: const Padding(
+                                  padding: EdgeInsets.all(4),
+                                  child: Icon(
+                                    Icons.moving_outlined,
+                                    size: 17,
+                                    color: AppColor.profitColor,
+                                  ),
+                                ),
+                              ),
+                              const Gap(8),
+                              Text(
+                                "20%",
+                                style: AppTextStyle.titleTextStyle.copyWith(
+                                  color: AppColor.mainWhiteColor,
+                                  fontSize: 17,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const Gap(4),
+                          Text(
+                            "Dibanding Kemarin",
+                            style: AppTextStyle.titleTextStyle.copyWith(
+                              color: AppColor.mainWhiteColor,
+                              fontSize: 11,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const Gap(14),
+              const Text(
+                "Aktivitas Terbaru",
+                style: AppTextStyle.titleTextStyle,
+              ),
+              const Gap(8),
+              _buildCard(
+                context,
+                Row(
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Pelanggan Hari Ini",
+                          style: AppTextStyle.titleTextStyle.copyWith(
+                            color: AppColor.mainWhiteColor,
+                            fontSize: 17,
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            const Icon(
+                              Icons.people,
+                              color: AppColor.mainWhiteColor,
+                              size: 32,
+                            ),
+                            const Gap(8),
+                            Text(
+                              "23",
+                              style: AppTextStyle.titleTextStyle.copyWith(
+                                color: AppColor.mainWhiteColor,
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    const Gap(22),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  border: Border.all(
+                                    color: AppColor.profitColor,
+                                    width: 1,
+                                  ),
+                                ),
+                                child: const Padding(
+                                  padding: EdgeInsets.all(4),
+                                  child: Icon(
+                                    Icons.moving_outlined,
+                                    size: 17,
+                                    color: AppColor.profitColor,
+                                  ),
+                                ),
+                              ),
+                              const Gap(8),
+                              Text(
+                                "20%",
+                                style: AppTextStyle.titleTextStyle.copyWith(
+                                  color: AppColor.mainWhiteColor,
+                                  fontSize: 17,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const Gap(4),
+                          Text(
+                            "Dibanding Kemarin",
+                            style: AppTextStyle.titleTextStyle.copyWith(
+                              color: AppColor.mainWhiteColor,
+                              fontSize: 11,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
+      ),
+    );
+  }
+
+  Container _buildCard(BuildContext context, Widget child) {
+    return Container(
+      width: MediaQuery.sizeOf(context).width,
+      decoration: BoxDecoration(
+        color: AppColor.mainOrangeColor,
+        borderRadius: BorderRadius.circular(18),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(22, 18, 22, 18),
+        child: child,
       ),
     );
   }
